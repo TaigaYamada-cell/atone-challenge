4f6ff516bf24cf7c495c4bfe8beab98b1bd92551
TaigaYamada-cell <daikonsukitarou@gmail.com>
2023-09-29 14:09:00 +0900

    Cardクラスとテストの作成

--- app/service/card.rb
+++ app/service/card.rb
@@ -1,5 +1,5 @@
 class Card
-  def self.calc_relative_value(num)
+  def calc_relative_value(num)
     if(num != 1)
       relative_value = num - 1
       return relative_value
@@ -10,9 +10,19 @@ class Card
   end
 
   def initialize(card_name_str)
+    n = card_name_str.length
+    str = card_name_str.slice(0)
+    num = card_name_str.slice(1, n).to_i
+    # 引数のバリデーション
+    if !(str == "H" || str == "C" || str == "D" || str == "S")
+      raise ArgumentError
+    elsif (num > 13 || num < 1)
+      raise ArgumentError
+    end
+
     # 文字列をsuitとnumに分割する
-    @suit = card_name_str.slice(0)
-    @num = card_name_str.slice(1, n).to_i
+    @suit = str
+    @num = num
     @relative_value = self.calc_relative_value(@num)
   end
 
