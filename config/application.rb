25d5c6f6f4108941b6debbcce8476c163fd85884
TaigaYamada-cell <daikonsukitarou@gmail.com>
2023-10-17 19:27:36 +0900

    リクエストのjsonにエラーがあった場合の処理を追加

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module AtoneTest
   class Application < Rails::Application
+    config.middleware.insert_before ActionDispatch::ParamsParser, 'RescueJsonParseErrors'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -22,5 +23,6 @@ module AtoneTest
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    # config.middleware.use "CatchJsonParseErrors"
  end
 end
